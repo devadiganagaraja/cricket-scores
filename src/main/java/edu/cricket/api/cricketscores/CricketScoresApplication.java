d9e4aa7aa95db2012cac1d41291115d30f1d6d2f
NAGARAJA DEVADIGA <nagaraja.devadiga@espn.com>
2019-06-05 12:27:53 +0530

    initial commit

---
+++ src/main/java/edu/cricket/api/cricketscores/CricketScoresApplication.java
@@ -0,0 +1,13 @@
+package edu.cricket.api.cricketscores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricketScoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricketScoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/cricket/api/cricketscores/CricketScoresApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.cricket.api.cricketscores;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CricketScoresApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
